435dbaff7a44e829cf13b6e3f8100cb3826cd277
IndunilRanganath <indunilranganath071@gmail.com>
2023-11-24 10:58:03 +0530

    logo changed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,8 @@ import {motion, AnimatePresence} from 'framer-motion';
 import {useSnapshot} from 'valtio';
 import state from '../store/index.js';
 import {CustomButton} from '../components';
+import  logo  from '../assets/396adz.png'
+
 
 
 import {
@@ -17,26 +19,34 @@ const Home = () => {
   return (
       <AnimatePresence>
         {snap.intro && (
-          <motion.section className='home' {...slideAnimation('left')}>
+          <motion.section className='home'
+          {...slideAnimation('left')}>
             <motion.header {...slideAnimation("down")} >
               <img
-                 src='./threejs.png'
+                 src={logo}
                  alt='logo'
-                 className='w-8 h-8 object-contain'
+                 className='w-100 h-12 object-contain'
               />
             </motion.header>
             <motion.div {...headContainerAnimation}>
               <motion.div {...headTextAnimation}>
-                <h1 className='head-text'>
-                  LET'S <br className='xl:block hidden' />DO IT.
-                </h1>
+              <h1 className="head-text"
+                  style={{
+                  fontSize: "clamp(3.5rem, 8vw, 8rem)",
+                  lineHeight: "clamp(4.5rem, 8vw, 9rem)",
+                  color: "#fff"
+                }}
+              >
+            LET'S <br className="xl:block hidden" /> CHOOSE IT.
+            </h1>
               </motion.div>
               
               <motion.div
               {...headContentAnimation}
               className="flex flex-col gap-5"
+              style={{ width: "clamp(20rem, 50vw, 40rem)" }}
               >
-              <p className="max-w-md font-normal text-gray-600 text-base">
+              <p className="max-w-md mt-10 mb-10 font-normal text-blue-400 text-base">
               Create your unique and exclusive shirt with our brand-new 3D customization tool. <strong>Unleash your imagination</strong>{" "} and define your own style.
               </p>
               </motion.div>
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,7 +2,7 @@ import { proxy } from "valtio";
 
 const store = proxy({
     intro : true,
-    color : '#EFBD48',
+    color : '#00AAFF',
     isLogoTexture : true,
     isFullTexture : false,
     logoDecal : './threejs.png',
