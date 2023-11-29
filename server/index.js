854ef98f346028f002943e847a05a2300042df80
IndunilRanganath <indunilranganath071@gmail.com>
2023-11-30 01:14:49 +0530

    OpenAI Part Backend Complected

--- server/index.js
+++ server/index.js
@@ -1,23 +1,19 @@
-import  Express  from "express";
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
 
-import * as dotenv from "dotenv";
-import cors from "cors";
-import dalleRoutes from "./routes/dalle.routes.js";
+import dalleRoutes from './routes/dalle.routes.js';
 
 dotenv.config();
 
-
-const app = Express();
+const app = express();
 app.use(cors());
-app.use(Express.json({limig: "50mb"}));
+app.use(express.json({ limig: "50mb" }))
 
-app.use('/api/v1/dalle', dalleRoutes);
+app.use("/api/v1/dalle", dalleRoutes);
 
-app.get("/", (req, res) => {
-    res.status(200).json({message: "Hello World"})
-});
-
-
-app.listen(8080, () => {
-    console.log("Server is running on port 8080");
+app.get('/', (req, res) => {
+  res.status(200).json({ message: "Hello from DALL.E" })
 })
+
+app.listen(8080, () => console.log('Server has started on port 8080'))
